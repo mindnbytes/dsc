79889e51c0c87e42ae1e982f05cf8e3e0a9e49b8
mindnbytes <mindnbytes@gmail.com>
2026-06-08 15:45:14 +0200

    Static grow, push, and move to header

--- list.c
+++ list.c
@@ -1,20 +1,32 @@
+/*
+ * List stores shallow copies of pushed elements.
+ * If an element contains pointers, the pointed-to data remains owned by the
+ * caller. list_clear only releases the list's internal storage.
+ */
+#include "list.h"
+
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 
-// Valid List invariant:
-//
-// - data == NULL iff cap == 0
-// - len <= cap
-// - if cap > 0, data points to cap elements
-// - elem_size > 0
-// - element = elem_size bytes of data
-typedef struct {
-  size_t cap;
-  size_t len;
-  size_t elem_size;
-  unsigned char *data;
-} List;
+// Private helper, grow internal array
+// return false if it fais, true otherwise
+static bool list_grow(List *list) {
+  // next list->cap will overflow
+  if (list->cap > SIZE_MAX / 2 / list->elem_size)
+    return false;
+  size_t new_cap = list->cap == 0 ? 8 : list->cap * 2;
+
+  unsigned char *new_data = realloc(list->data, new_cap * list->elem_size);
+  if (new_data == NULL)
+    return false;
+
+  list->data = new_data;
+  list->cap = new_cap;
+
+  return true;
+}
 
 // List initializer
 // fails on NULL pointer or 0 elem_size
@@ -28,6 +40,26 @@ bool list_init(List *l, size_t elem_size) {
   l->elem_size = elem_size;
   l->data = NULL;
 
+  return true;
+}
+
+// Push the shallow copy of the element
+// on the list.
+// - returns false on failure
+// - returns true on success
+bool list_push(List *l, const void *val) {
+  if (l == NULL || val == NULL)
+    return false;
+  if (sizeof(*val) != l->elem_size)
+    return false;
+  if (l->len == l->cap) {
+    if (!list_grow(l))
+      return false;
+  }
+  // copy bytes
+  unsigned char *dst = l->data + l->len * l->elem_size;
+  memcpy(dst, val, l->elem_size);
+  l->len++;
   return true;
 }
 
---
+++ list.h
@@ -0,0 +1,25 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdbool.h>
+#include <stddef.h>
+
+// Valid List invariant:
+//
+// - data == NULL iff cap == 0
+// - len <= cap
+// - if cap > 0, data points to cap elements
+// - elem_size > 0
+// - element = elem_size bytes of data
+typedef struct {
+  size_t cap;
+  size_t len;
+  size_t elem_size;
+  unsigned char *data;
+} List;
+
+bool list_init(List *l, size_t elem_size);
+bool list_push(List *l, const void *val);
+bool list_clear(List *l);
+
+#endif

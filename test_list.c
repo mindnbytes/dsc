59a62312fd0fb4d762746ede291aeae700e8b681
mindnbytes <mindnbytes@gmail.com>
2026-06-09 13:15:05 +0200

    Init fail test both fail modes + init pass test

--- test_list.c
+++ test_list.c
@@ -10,13 +10,25 @@
     fn();                                                                      \
   } while (0)
 
-static void test_init_fail_null(void) {
-  List *list = NULL;
-  assert(!list_init(list, 2));
+static void test_init_pass(void) {
+  List a;
+  assert(list_init(&a, 2));
+  assert(a.cap == 0);
+  assert(a.len == 0);
+  assert(a.elem_size == 2);
+}
+
+static void test_init_fail(void) {
+  List *a = NULL;
+  List b;
+  assert(!list_init(a, 2));
+  assert(!list_init(&b, 0));
 }
 
 int main(void) {
-  RUN_TEST(test_init_fail_null);
+  RUN_TEST(test_init_pass);
+  RUN_TEST(test_init_fail);
+
   puts("All tests passed.");
   return EXIT_SUCCESS;
 }
